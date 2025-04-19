d23e2f0716c8d1cf5c9c42c7e2cb620f1b6fc109
foxtrot47 <jjneutron@outlook.com>
2025-04-19 18:07:08 +0530

    feat: layout fixes

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -2,8 +2,8 @@ import { Link, Outlet } from "react-router";
 
 export default function Layout() {
   return (
-    <div className="bg-white w-full rounded-t-3xl shadow-lg h-screen overflow-hidden">
-      <div className="flex justify-between items-center p-4 border-b">
+    <div className="bg-white w-full rounded-t-3xl shadow-lg overflow-hidden" style={{ maxHeight: "100vh"}}>
+      <div className="flex justify-between items-center p-4 border-b max-h-16">
         <div className="flex items-center">
           <div className="bg-orange-500 rounded-full p-2 mr-2">
             <svg
@@ -14,9 +14,9 @@ export default function Layout() {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M13 10V3L4 14h7v7l9-11h-7z"
               />
             </svg>
@@ -36,9 +36,9 @@ export default function Layout() {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
               />
             </svg>
@@ -65,16 +65,16 @@ export default function Layout() {
               </Link>
             </li>
             <li className="px-4 py-3 mx-2 mb-2 text-gray-600 hover:bg-gray-100 rounded-lg">
-              <a href="#" className="flex items-center gap-3">
+              <Link to={"/tables"} className="flex items-center gap-3">
                 <i className="fas fa-utensils"></i>
                 <span>Table</span>
-              </a>
+              </Link>
             </li>
             <li className="px-4 py-3 mx-2 mb-2 text-gray-600 hover:bg-gray-100 rounded-lg">
-              <a href="#" className="flex items-center gap-3">
+              <Link to={"/reports"} className="flex items-center gap-3">
                 <i className="fas fa-users"></i>
                 <span>Reports</span>
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
